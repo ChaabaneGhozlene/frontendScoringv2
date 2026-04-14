b1188ba816070f18fbbe02a5dfec94b413a3d283
ghozlene chaabane <chaabaneghozlene@gmaiol.com>
2026-04-14 16:52:07 +0100

    Ghozlene le 14-04-2026

--- src/Redux/reducers.ts
+++ src/Redux/reducers.ts
@@ -1,9 +1,11 @@
 // src/Redux/reducers.ts
 import { combineReducers } from 'redux';
 import authReducer from './auth/reducers'; // ✅ make sure this is imported
-
+import themeConfigReducer from '../store/themeConfigSlice'; // ✅ ajoute ceci
 const rootReducer = combineReducers({
   auth: authReducer, // ✅ key must match what you use in selectors
+    themeConfig: themeConfigReducer, // ✅ ajoute ceci
+
 });
 
 export default rootReducer;
