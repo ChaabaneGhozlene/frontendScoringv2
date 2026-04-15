ea5e65ba1467f186647c2536a5bf93e95cf1af3b
ghozlene chaabane <chaabaneghozlene@gmaiol.com>
2026-04-15 20:26:43 +0100

    Ghozlene le 14-04-2026

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
 import store from './Redux/store'
+import './tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4361ee',
+        secondary: '#805dca',
+        success: '#00ab55',
+        danger: '#e7515a',
+        warning: '#e2a03f',
+        info: '#2196f3',
+        dark: '#3b3f5c',
+        'white-light': '#e0e6ed',
+        'white-dark': '#888ea8',
+        'dark-light': '#eaeaec',
+        'black': '#0e1726',
+      },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
